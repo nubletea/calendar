c8f534e0b60b0d8232dff9ff20a4a6fb804e1c5e
nubletea <71910560+nubletea@users.noreply.github.com>
2020-11-04 23:28:46 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -28,15 +28,16 @@
     function date(){
         let this_day=new Date(today.getFullYear(),today.getMonth()+day_count.count-1,1);
         let this_month=new Date(today.getFullYear(),today.getMonth()+day_count.count,0);
+        let prev_month=new Date(today.getFullYear(),today.getMonth()+day_count.count-1,0);
         day_count.text="";
         day_count.arr=[];
-        for(let j=0;j<6;j++){
-            if(this_day.getDay()>j){
-            day_count.arr[j]="<span></span>";
+        for(let i=0;i<6;i++){
+            if(this_day.getDay()>i){
+            day_count.arr[i]="<span class='prev'>"+((prev_month.getDate()-this_day.getDay()+1)+i)+"</span>";
             }
         }
-        for(let i=0;i<this_month.getDate();i++){
-            day_count.arr[i+this_day.getDay()]="<span>"+(i+1)+"</span>"
+        for(let j=0;j<this_month.getDate();j++){
+            day_count.arr[j+this_day.getDay()]="<span>"+(j+1)+"</span>"
         };
         for(let k=0;k<day_count.arr.length;k++){
             if((k+1)%7===0){
