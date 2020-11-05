698abb6cf5e27eaefdf1c99b3094484c58601aae
nubletea <71910560+nubletea@users.noreply.github.com>
2020-11-05 12:34:09 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
    const DAY=document.querySelector('.day');
    const YEAR=document.querySelector('.year');
    const MONTH=document.querySelector('.month_change');
+   const DAY_SPAN=document.querySelectorAll('.day span');
    const today=new Date();
    const day_count={
        count:1,
@@ -23,7 +24,6 @@
     });
     date();
 });
-
     // day //
     function date(){
         let this_day=new Date(today.getFullYear(),today.getMonth()+day_count.count-1,1);
@@ -37,13 +37,24 @@
             }
         }
         for(let j=0;j<this_month.getDate();j++){
+          if(today.getDate()===j+1&&today.getMonth()===this_month.getMonth()){
+            day_count.arr[j+this_day.getDay()]="<span class='active'>"+(j+1)+"</span>"
+          }else{
             day_count.arr[j+this_day.getDay()]="<span>"+(j+1)+"</span>"
+          }
         };
         for(let k=0;k<day_count.arr.length;k++){
             if((k+1)%7===0){
-            day_count.text+=day_count.arr[k]+'<br>';
+              day_count.arr[k]="<span class='sat'>"+(k+1-this_day.getDay())+"</span>"
+              day_count.text+=day_count.arr[k]+'<br>';
+            }else if((k+1)%7===1&&k+1!==1){
+              day_count.arr[k]="<span class='sun'>"+(k+1-this_day.getDay())+"</span>"
+              day_count.text+=day_count.arr[k];
+            }else if(day_count.arr[0]==='<span>1</span>'){
+              day_count.arr[k]="<span class='sun'>"+(k+1-this_day.getDay())+"</span>"
+              day_count.text+=day_count.arr[k];
             }else{
-            day_count.text+=day_count.arr[k];
+              day_count.text+=day_count.arr[k];
             }
         }
     DAY.innerHTML=day_count.text;
